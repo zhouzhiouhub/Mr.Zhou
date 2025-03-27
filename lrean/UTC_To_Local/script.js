d192e8f71b479b89169e21f7f84a51f818b1eb00
ZhouZhenYun <2922188469@qq.com>
2025-03-27 20:05:25 +0800

    加注释

--- lrean/UTC_To_Local/script.js
+++ lrean/UTC_To_Local/script.js
@@ -7,8 +7,14 @@
     dayjs.extend(dayjs_plugin_utc);
 
     function updateTime() {
+        // 获取当前 UTC 时间并格式化为字符串
+        // 使用 UTC 插件获取当前 UTC 时间
         const currentUTCTime = dayjs.utc();
+        //获取UTC时间
+        // 使用 format() 方法将时间格式化为字符串
         document.getElementById("utc-time").textContent = currentUTCTime.format('YYYY-MM-DD HH:mm:ss');
+        //获取本地时间
+        // 使用 local() 方法获取本地时间并格式化为字符串
         document.getElementById("local-time").textContent = currentUTCTime.local().format('YYYY-MM-DD HH:mm:ss');
     }
     updateTime();
